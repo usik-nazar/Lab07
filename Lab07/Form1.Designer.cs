25f2be5fa2be41d642ca4b93120594d51309200f
usik-nazar <nazar.usick200@gmail.com>
2020-12-09 20:55:37 +0200

    Добавьте файлы проекта.

---
+++ Lab07/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Lab07
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tBX1 = new System.Windows.Forms.TextBox();
+            this.tBX2 = new System.Windows.Forms.TextBox();
+            this.tBY = new System.Windows.Forms.TextBox();
+            this.lblY = new System.Windows.Forms.Label();
+            this.lblX2 = new System.Windows.Forms.Label();
+            this.lblX1 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tBX1
+            // 
+            this.tBX1.Location = new System.Drawing.Point(82, 28);
+            this.tBX1.Name = "tBX1";
+            this.tBX1.Size = new System.Drawing.Size(201, 23);
+            this.tBX1.TabIndex = 0;
+            this.tBX1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tBX2
+            // 
+            this.tBX2.Location = new System.Drawing.Point(82, 76);
+            this.tBX2.Name = "tBX2";
+            this.tBX2.Size = new System.Drawing.Size(201, 23);
+            this.tBX2.TabIndex = 0;
+            this.tBX2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tBY
+            // 
+            this.tBY.Location = new System.Drawing.Point(82, 125);
+            this.tBY.Name = "tBY";
+            this.tBY.ReadOnly = true;
+            this.tBY.Size = new System.Drawing.Size(201, 23);
+            this.tBY.TabIndex = 0;
+            this.tBY.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.Location = new System.Drawing.Point(34, 128);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(14, 15);
+            this.lblY.TabIndex = 1;
+            this.lblY.Text = "Y";
+            // 
+            // lblX2
+            // 
+            this.lblX2.AutoSize = true;
+            this.lblX2.Location = new System.Drawing.Point(34, 79);
+            this.lblX2.Name = "lblX2";
+            this.lblX2.Size = new System.Drawing.Size(20, 15);
+            this.lblX2.TabIndex = 1;
+            this.lblX2.Text = "X2";
+            // 
+            // lblX1
+            // 
+            this.lblX1.AutoSize = true;
+            this.lblX1.Location = new System.Drawing.Point(34, 31);
+            this.lblX1.Name = "lblX1";
+            this.lblX1.Size = new System.Drawing.Size(20, 15);
+            this.lblX1.TabIndex = 1;
+            this.lblX1.Text = "X1";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(82, 173);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(91, 24);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(227, 173);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(78, 24);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(289, 123);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(74, 24);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 218);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblX1);
+            this.Controls.Add(this.lblX2);
+            this.Controls.Add(this.lblY);
+            this.Controls.Add(this.tBY);
+            this.Controls.Add(this.tBX2);
+            this.Controls.Add(this.tBX1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tBX1;
+        private System.Windows.Forms.TextBox tBX2;
+        private System.Windows.Forms.TextBox tBY;
+        private System.Windows.Forms.Label lblY;
+        private System.Windows.Forms.Label lblX2;
+        private System.Windows.Forms.Label lblX1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Lab07/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            TryCalculate(tBX1.Text, tBX2.Text, out string strY);
+            tBY.Text = strY;
+        }
+        private bool TryCalculate(string strX1, string strX2, out string strY)
+        {
+            var r1 = double.TryParse(strX1, out double x1);
+            var r2 = double.TryParse(strX2, out double x2);
+
+            if (!r1 || !r2)
+            {
+                strY = "Can't calculate!";
+                return false;
+            }
+            var y = Func(x1, x2);
+            strY = string.Format("{0:###.####E+000}", y);
+            return true;
+        }
+
+        private double Func(double x1, double x2)
+        {
+            double s = Math.Sin(x2);
+            double f1 = 45 * x1 * s;
+            double p = Math.Pow(x1, 3);
+            double f2 = Math.Sqrt(9 * x2 * p);
+            return f1 + f2;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tBX1.Text = "";
+            tBX2.Text = "";
+            tBY.Text = "";
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
